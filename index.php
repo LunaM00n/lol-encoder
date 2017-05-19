ea694461f78717fe1d73e66080c5ef92d93954e0
Thin Ba Shane (Luna) <thinbashane@gmail.com>
2017-05-19 23:30:20 +0630

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 <h1> This is the raw example </h1>
 
-<fieldset>Concat Function (eisikway)<br>
+<fieldset><b>Concat Function</b> (eisikway)<br>
 String 1<input type="text" id="firstname">
 <br>
 String 2<input type="text" id="lastname">
@@ -23,6 +23,24 @@ var lname = document.getElementById('lastname').value;
 var symbol=prompt("Symbol?");
 var name=fname.concat(symbol,lname);
 document.getElementById('name').value=name;
+}
+
+</script>
+
+<fieldset><b>Encoding Function</b> (@art0flunam00n)<br>
+String<br><textarea id="in"></textarea>
+<button type="button" onclick="base64encode()">b64->en</button>
+<button type="button" onclick="base64decode()">b64->en</button><br>
+</fieldset>
+
+<script>
+function base64encode(){
+	var input=document.getElementById('in').value;
+	document.getElementById('in').value=btoa(input);
+}
+function base64decode(){
+	var input=document.getElementById('in').value;
+	document.getElementById('in').value=atob(input);
 }
 
 </script>
