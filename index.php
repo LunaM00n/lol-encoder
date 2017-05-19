2a68ebbc17770c76d7d32b6fe0f5f8d621e3b87f
Thin Ba Shane (Luna) <thinbashane@gmail.com>
2017-05-19 21:42:30 +0630

    Add files via upload
    Example added base64 encode / decode

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOL Encoder</title>
+</head>
+<body>
+
+<h1> This is the raw example </h1>
+
+<textarea id="input">
+342 Alvin Road
+Ducksburg</textarea>
+
+
+<button type="button" onclick="base64EncodeFunction()">b64->en</button>
+<button type="button" onclick="base64DecodeFunction()">b64->de</button>
+
+<script>
+var input=document.getElementById("input").value;
+
+function base64EncodeFunction(){
+	document.getElementById("input").value = btoa(input);
+}
+
+function base64DecodeFunction(){
+	document.getElementById("input").value = atob(input);
+}
+</script>
+</body>
+</html>
